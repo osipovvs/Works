b24b58df2346ca0dcbe81fed1b30a6ce12993348
Vasily Osipov <osipov1140@gmail.com>
2017-09-29 13:41:29 +0300

    First Python program written at home

---
+++ python1.py
@@ -0,0 +1,4 @@
+import math
+kat1 = float(input('Enter the first cathetus length: '))
+kat2 = float(input('Enter the second cathetus length: '))
+print('The length of hypotenuse is ', math.sqrt(kat1**2 + kat2**2), ' units, and the area is', (kat1 * kat2) / 2, ' sq. units.')
