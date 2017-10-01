8d093b0a65890bcb07f80acb75305b974df69fc6
Vasily Osipov <osipov1140@gmail.com>
2017-10-01 22:43:03 +0300

    Modified 1st task

--- task_01_01.py
+++ task_01_01.py
@@ -2,7 +2,8 @@ area_cent = float(input())
 area_meters = area_cent * 100
 bed_length = 25
 bed_width = 15
+bed_area = bed_length * bed_width
 
-free_area = int(area_meters - (bed_length * bed_width))
+free_area = int(area_meters % bed_area)
 
-print(free_area) if free_area > 0 else print(0)
+print(free_area)
