6f139b56717bed4adf4d3e64568208308409fb3d
Vasily Osipov <osipov1140@gmail.com>
2017-10-03 15:05:03 +0300

    Modified to ignore bad characters

--- task_02_01.py
+++ task_02_01.py
@@ -1,7 +1,24 @@
 def is_palindrome(s):
     res = False
     s2 = s.replace(' ', '')
-    s3 = s2.replace(',', '')
+
+    bad_chars = {33: None, 
+    34: None,
+    44: None,
+    45: None,
+    46: None,
+    47: None,
+    58: None,
+    59: None,
+    63: None,
+    96: None,
+    145: None,
+    146: None,
+    150: None,
+    151: None
+}
+
+    s3 = s2.translate(bad_chars)
     s_fin = s3.lower()
     if s_fin[::-1] == s_fin:
         res = True
