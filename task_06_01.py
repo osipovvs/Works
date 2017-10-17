adedd20c6b2845bc13c77db2ed929269a59278bf
Vasily Osipov <osipov1140@gmail.com>
2017-10-18 02:14:03 +0300

    Homework on decorators v2, mistake corrected

--- task_06_01.py
+++ task_06_01.py
@@ -1,44 +1,28 @@
 import platform
 
-#print(platform.system(), type(platform.system()))
-
 def run_on_linux(func):
     def wrapper(*args, **kwargs):
         os = platform.system()
-        result = func(*args, **kwargs)
-        if os != 'Linux':
-            return None
-        return result
+        return func(*args, **kwargs) if os == 'Linux' else None
     return wrapper
 
 
 def run_on_windows(func):
     def wrapper(*args, **kwargs):
         os = platform.system()
-        result = func(*args, **kwargs)
-        if os != 'Windows':
-            return None
-        return result
+        return func(*args, **kwargs) if os == 'Windows' else None
     return wrapper
 
 def run_on_macos(func):
     def wrapper(*args, **kwargs):
         os = platform.system()
-        result = func(*args, **kwargs)
-        if os != 'Darwin':
-            return None
-        return result
+        return func(*args, **kwargs) if os == 'Darwin' else None
     return wrapper
 
 if __name__ == '__main__':
-    @run_on_windows
+    @run_on_macos
     def func():
         print("This function can be run only on Windows!")
     
+    
     func()
-
-# os = platform.system()
-# if os == 'Windows':
-#     print('It is Windows')
-# else:
-#     print('It is not Windows')
